d2250484c222211e2694b25d60287f3af1892d2a
juliya <juliya@MacBook-Air-juliya.local>
2023-10-28 21:37:24 +0200

    feat: add page markup

---
+++ src/components/Results.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Results() {
+  return (
+    <div className="Results">
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Results;
