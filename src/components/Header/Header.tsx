9a816a783c6a4a21c5356cb1eaba5bbfefc59ac8
juliya <juliya@MacBook-Air-juliya.local>
2023-11-05 18:28:13 +0100

    fix: logo path

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 function Header() {
   return (
     <header className={styles.app_header}>
-      <img src="/public/img/Star_Wars_Logo.png" alt="Logo" width={250} />
+      <img src="img/Star_Wars_Logo.png" alt="Logo" width={250} />
       <h1 className={styles.header_title}>Search App</h1>
     </header>
   );
